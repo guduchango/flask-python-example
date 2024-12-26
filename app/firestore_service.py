941c967feab1278054a7173e8bd13ec664873e84
Edgardo Ponce <ed.poncepaez@gmail.com>
2024-12-26 12:05:54 -0300

    last modification before copy platzi files from source

--- app/firestore_service.py
+++ app/firestore_service.py
@@ -1,11 +1,21 @@
 import firebase_admin
 from firebase_admin import credentials,firestore
 
+project_id = "platzi-flask-445912"
 credentials = credentials.ApplicationDefault()
-firebase_admin.initialize_app(credentials)
+firebase_admin.initialize_app(credentials,{
+    'projectId': project_id
+})
 
 db = firestore.client()
 
 def get_users():
-    return db.collection('users').get()
+    users = db.collection('users').get()
+    print(f"{users}")
+    return users
 
+def get_todos(user_id):
+    return 
+    db.collection('users')\
+    .document(user_id)\
+    .collection('todos').get()
